b0d6370f5c73948d7457964e954f5597297024a3
Ruttika Jadhav <ruttikajadhav@gmail.com>
2025-05-12 14:27:44 +0530

    New changes

--- src/Books.js
+++ src/Books.js
@@ -1,18 +1,26 @@
 import React from "react";
+import BookTag from "../src/image/books.png";
 
 export default function Books({ books, deleteBook }) {
-  return books.map((book) => (
-    <div className="book-png" key={book.id}>
-      <p>
-        Name of book: <strong>{book.bookName}</strong>
-      </p>
-      <p>
-        Page Number: <strong>{book.pageNumber}</strong>
-      </p>
-      <div className="clear" onClick={() => deleteBook(book.id)}>
-        <ion-icon name="close-outline"></ion-icon>
-      </div>
-      <div className="break"></div>
-    </div>
-  ));
+	return books.map((book) => (
+		<div className='book-png' key={book.id}>
+			<img
+				src={BookTag}
+				alt='books-tag'
+				style={{ height: "5rem", width: "5rem" }}
+			/>
+			<section>
+				<p>
+					Name of book: <strong>{book.bookName}</strong>
+				</p>
+				<p>
+					Page Number: <strong>{book.pageNumber}</strong>
+				</p>
+			</section>
+
+			<div className='clear' onClick={() => deleteBook(book.id)}>
+				<ion-icon name='close-outline'></ion-icon>
+			</div>
+		</div>
+	));
 }
--- src/MainContent.js
+++ src/MainContent.js
@@ -1,135 +1,125 @@
 import React from "react";
 import Books from "./Books";
 import { nanoid } from "nanoid";
+import Bookmark from "../src/image/Bookmarks.jpg";
 
 // Getting data
 
 const collectedInfo = () => {
-  const info = localStorage.getItem("books");
-  if (info) {
-    return JSON.parse(info);
-  } else {
-    return [];
-  }
+	const info = localStorage.getItem("books");
+	if (info) {
+		return JSON.parse(info);
+	} else {
+		return [];
+	}
 };
 
 function MainContent() {
-  const [bookName, setBookName] = React.useState("");
-  const [pageNumber, setPageNumber] = React.useState("");
-  const [id, setId] = React.useState("");
+	const [bookName, setBookName] = React.useState("");
+	const [pageNumber, setPageNumber] = React.useState("");
+	const [id, setId] = React.useState("");
 
-  const [books, setBooks] = React.useState(collectedInfo());
+	const [books, setBooks] = React.useState(collectedInfo());
 
-  const handleBookName = function (e) {
-    setBookName(e.target.value);
-  };
+	const handleBookName = function (e) {
+		setBookName(e.target.value);
+	};
 
-  const deleteBook = (id) => {
-    const filteredBooks = books.filter((element, index) => {
-      return element.id !== id;
-    });
-    setBooks(filteredBooks);
-  };
+	const deleteBook = (id) => {
+		const filteredBooks = books.filter((element, index) => {
+			return element.id !== id;
+		});
+		setBooks(filteredBooks);
+	};
 
-  const handlePageNumber = function (e) {
-    setPageNumber(e.target.value);
-  };
+	const handlePageNumber = function (e) {
+		setPageNumber(e.target.value);
+	};
 
-  const onSubmit = function (e) {
-    e.preventDefault();
+	const onSubmit = function (e) {
+		e.preventDefault();
 
-    let book = {
-      bookName,
-      pageNumber,
-      id: nanoid(),
-    };
-    setBooks([...books, book]);
+		let book = {
+			bookName,
+			pageNumber,
+			id: nanoid(),
+		};
+		setBooks([...books, book]);
 
-    setBookName("");
-    setPageNumber("");
-    setId("");
-  };
+		setBookName("");
+		setPageNumber("");
+		setId("");
+	};
 
-  const pageIntro = function () {
-    return (
-      <div className="intro">
-        <h2 className="title">Lesemarke</h2>
-        <p className="text">
-          Love reading too much books & not able to bookmark them physically?
-        </p>
-        <p className="text">
-          I always run into that kind of problem, lesemarke app will help you
-          keep track on those plenty of forgettable page numbers with virtual
-          bookmarks.
-        </p>
-        <p className="text">
-          This app is made by using <span className="underlined">React</span>.
-        </p>
-      </div>
-    );
-  };
+	const pageIntro = function () {
+		return (
+			<div className='intro'>
+				<h2 className='title'>Lesemarke</h2>
+				<p className='text'>
+					Love reading too much books & not able to bookmark them
+					physically?
+				</p>
+				<p className='text'>
+					I always run into that kind of problem, lesemarke app will
+					help you keep track on those plenty of forgettable page
+					numbers with virtual bookmarks.
+				</p>
+				<p className='text'>
+					This app is made by using{" "}
+					<span className='underlined'>React</span>.
+				</p>
+			</div>
+		);
+	};
 
-  // Saving data
-  React.useEffect(() => {
-    localStorage.setItem("books", JSON.stringify(books));
-  }, [books]);
+	// Saving data
+	React.useEffect(() => {
+		localStorage.setItem("books", JSON.stringify(books));
+	}, [books]);
 
-  return (
-    <div className="App">
-      <div className="MainContent">
-        <div className="img-container">
-          <img
-            className="bookmark-img"
-            src="/images/Bookmarks.jpg"
-            alt="Photo by pure julia on Unsplash"
-          />
-        </div>
-      </div>
+	return (
+		<div className='App'>
+			<div className='MainContent'>
+				<form className='card' onSubmit={onSubmit}>
+					<section className='input-field-section'>
+						<label>Book Name :</label>
+						<input
+							type='text'
+							onChange={handleBookName}
+							name='bookName'
+							value={bookName}
+							placeholder='Enter book Name'
+							required
+						/>
+					</section>
+					<section className='input-field-section'>
+						<label>Page Number :</label>
+						<input
+							type='number'
+							onChange={handlePageNumber}
+							name='pageNumber'
+							value={pageNumber}
+							placeholder='Enter page Number'
+							required
+							min='1'
+						/>
+					</section>
 
-      <div className="form-container centered">
-        <form className="card" onSubmit={onSubmit}>
-          <div className="in-el">
-            <label>
-              Book Name :
-              <input
-                type="text"
-                onChange={handleBookName}
-                name="bookName"
-                value={bookName}
-                placeholder="Enter book Name"
-                required="true"
-              />
-            </label>
-            <br />
-            <label>
-              Page Number :
-              <input
-                type="number"
-                onChange={handlePageNumber}
-                name="pageNumber"
-                value={pageNumber}
-                placeholder="Enter page Number"
-                required="true"
-              />
-            </label>
-          </div>
-          <div>
-            <button className="submit"> Save</button>
-          </div>
-        </form>
-      </div>
+					<button className='submit'> Save</button>
+				</form>
+			</div>
 
-      <div className="Info">
-        <div className="bookmarked-page">
-          {books.length === 0 ? (
-            pageIntro()
-          ) : (
-            <Books books={books} deleteBook={deleteBook} />
-          )}
-        </div>
-      </div>
-    </div>
-  );
+			<div className='Info'>
+				{books.length === 0 ? (
+					<div className='bookmarked-page'>{pageIntro()}</div>
+				) : (
+					<div className='books'>
+						<Books books={books} deleteBook={deleteBook} />
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default MainContent;
